a51f2a7d032690a7139a12ec32b0bec2a0b5dbeb
Juhana Autti <ext-juhana.autti@3stepit.com>
2024-12-14 08:12:21 +0200

    reformat aocHelpers

--- aocHelpers/aocHelpers/decorators.py
+++ aocHelpers/aocHelpers/decorators.py
@@ -2,24 +2,43 @@ import logging
 from time import perf_counter
 
 
-def timer(func):
-	'''Decorator that logs the execution time'''
-	
-	def wrap(*args, **kwargs):
-		start = perf_counter()
-		result = func(*args, **kwargs)
-		end = perf_counter()
+def timer_print(func):
+    """Decorator that prints the execution time"""
 
-		logging.info(f"{(end - start )* 1000} ms")
-		
-		return result
-	return wrap
+    def wrap(*args, **kwargs):
+        start = perf_counter()
+        result = func(*args, **kwargs)
+        end = perf_counter()
+
+        print(f"{(end - start )* 1000} ms")
+
+        return result
+
+    return wrap
+
+
+def timer(func):
+    """Decorator that logs the execution time"""
+
+    def wrap(*args, **kwargs):
+        start = perf_counter()
+        result = func(*args, **kwargs)
+        end = perf_counter()
+
+        logging.info(f"{(end - start )* 1000} ms")
+
+        return result
+
+    return wrap
+
 
 def print_result(func):
-	'''Decorator that prints the result of the executed function'''
-	def wrap(*args, **kwargs):
-		result = func(*args, **kwargs)
-		logging.info(f"{func.__name__}:")
-		logging.info(f"{result = }")
-		return result
-	return wrap
+    """Decorator that prints the result of the executed function"""
+
+    def wrap(*args, **kwargs):
+        result = func(*args, **kwargs)
+        logging.info(f"{func.__name__}:")
+        logging.info(f"{result = }")
+        return result
+
+    return wrap
