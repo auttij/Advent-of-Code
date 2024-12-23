9c2824d3e84a6dc322f813ed9673b280ed4a2b84
Juhana Autti <ext-juhana.autti@3stepit.com>
2024-12-23 08:07:07 +0200

    add 2024/23 solution

---
+++ 2024/23/day.py
@@ -0,0 +1,72 @@
+from sys import argv
+from os import path
+from aocHelpers import inputs
+from aocHelpers.decorators import timer, print_result
+from aocHelpers.init import init
+from itertools import combinations
+
+
+def parse(arr):
+    global neighbors
+    neighbors = {}
+    pairs = [tuple(i.split("-")) for i in arr]
+    for a, b in pairs:
+        if a not in neighbors:
+            neighbors[a] = []
+        neighbors[a].append(b)
+        if b not in neighbors:
+            neighbors[b] = []
+        neighbors[b].append(a)
+
+
+@timer
+@print_result
+def part1():
+    candidates = [node for node in neighbors.keys() if node[0] == "t"]
+    triplets = set()
+
+    for a in candidates:
+        n_a = neighbors[a]
+        for b in n_a:
+            n_b = neighbors[b]
+            for c in n_b:
+                if c in n_a:
+                    triplets.add(tuple(sorted((a, b, c))))
+    return len(triplets)
+
+
+def is_clique(nodes):
+    return all(
+        node in neighbors[other] for node in nodes for other in nodes if node != other
+    )
+
+
+def find_largest_clique():
+    candidates = [node for node in neighbors.keys() if node[0] == "t"]
+    largest_clique = []
+
+    for node in candidates:
+        n_a = neighbors[node]
+        for sub in range(1, len(n_a) + 1):
+            for combo in combinations(n_a, sub):
+                clique = (node,) + combo
+                if is_clique(clique) and len(clique) > len(largest_clique):
+                    largest_clique = list(clique)
+    return largest_clique
+
+
+@timer
+@print_result
+def part2():
+    return ",".join(sorted(find_largest_clique()))
+
+
+def main(args=None):
+    arr = init(path.dirname(__file__), inputs.read_to_str_arr, args)
+    parse(arr)
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main(argv[1:])
