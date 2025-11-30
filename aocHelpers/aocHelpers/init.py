bec71ec9d9b6b9763dd317ef69997519ba65db86
Juhana Autti <juhana.autti@gmail.com>
2025-11-30 19:07:15 +0200

    simplify aocHelpers

--- aocHelpers/aocHelpers/init.py
+++ aocHelpers/aocHelpers/init.py
@@ -1,47 +1,38 @@
-import sys
 import argparse
 import logging
 from os import path
 
+
 def get_arg_parser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('input', type=int, default=1, choices=range(1,5), help='Used input file number')
-	parser.add_argument('--logging', '-l', type = str.lower, default="info", choices=["debug", "info", "warn", "error", "critical"])
-	return parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "input", type=int, default=1, nargs="?", help="input file number"
+    )
+    parser.add_argument(
+        "--log",
+        "-l",
+        default="info",
+        choices=["debug", "info", "warn", "error"],
+    )
+    return parser
 
-def init_logging(log_path, log_key = "info"):
-	log_file = path.join(log_path, "log.log")
-	log_level_map = { 
-		"debug": logging.DEBUG, 
-		"info": logging.INFO,
-		"warn": logging.WARN,
-		"error": logging.ERROR,
-		"critical": logging.CRITICAL}
 
-	log_level = log_level_map[log_key]
+def init_logging(level):
+    logging.basicConfig(
+        level=level.upper(),
+        format="[%(levelname)s] %(message)s",
+    )
 
-	logging.basicConfig(
-		level=log_level, 
-		format="[%(levelname)s] %(message)s",
-		handlers=[
-			logging.FileHandler(log_file),
-			logging.StreamHandler(sys.stdout)
-	]
-	)
-	logging.info(f"writing logs to {log_file = } with {log_key = }")
 
-def init(__dir__, input_func, args=None):
-	# Create arg parser
-	parser = get_arg_parser()
+def init(dirpath, args=None):
+    parser = get_arg_parser()
+    pargs = parser.parse_args(args)
 
-	# parse args
-	pargs = parser.parse_args(args)
+    init_logging(pargs.log)
 
-	# init logging
-	init_logging(__dir__, pargs.logging)
+    # determine input file
+    filename = path.join(dirpath, f"input{pargs.input}.txt")
+    logging.info(f"Using input: {filename}")
 
-	# determine input file
-	filename = path.join(__dir__, f"input{pargs.input}.txt")
-	logging.info(f"using input {filename = }")
-	logging.info(f"reading input using {input_func.__name__}")
-	return input_func(filename)
+    with open(filename) as f:
+        return f.read()
