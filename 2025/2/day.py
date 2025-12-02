fe1e8fd687cf09a5a88dd2b07c01dd32fd770ca3
Juhana Autti <juhana.autti@gmail.com>
2025-12-02 07:26:26 +0200

    add 2025/2 solution

---
+++ 2025/2/day.py
@@ -0,0 +1,53 @@
+from sys import argv
+from os import path
+from aocHelpers.decorators import aoc_part
+from aocHelpers.init import init
+
+
+def check_id_list(data, check_fn):
+    sum = 0
+    for a, b in data:
+        for i in range(int(a), int(b) + 1):
+            if not check_fn(str(i)):
+                sum += i
+    return sum
+
+
+@aoc_part
+def part1(arr):
+    def is_valid(id):
+        l = len(id)
+        return id[: l // 2] != id[l // 2 :]
+
+    return check_id_list(arr, is_valid)
+
+
+@aoc_part
+def part2(arr):
+    def is_valid(id):
+        for i in range(1, len(id) // 2 + 1):
+            if not len(id) % i and id.count(id[:i]) == (len(id) / i):
+                return False
+        return True
+
+    return check_id_list(arr, is_valid)
+
+
+def parse_input(raw):
+    return [
+        pair.split("-") for pair in [line.split(",") for line in raw.splitlines()][0]
+    ]
+
+
+def main(args=None):
+    raw = init(path.dirname(__file__), args)
+
+    data1 = parse_input(raw)
+    part1(data1)
+
+    data2 = parse_input(raw)
+    part2(data2)
+
+
+if __name__ == "__main__":
+    main(argv[1:])
