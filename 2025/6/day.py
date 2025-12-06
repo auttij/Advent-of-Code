6d0dc8da38f2d556e3713d81b30d314dcef425f0
Juhana Autti <juhana.autti@gmail.com>
2025-12-06 07:57:09 +0200

    simplify 2025/6

--- 2025/6/day.py
+++ 2025/6/day.py
@@ -1,10 +1,15 @@
 from sys import argv
 from os import path
+from aocHelpers.helpers import transpose
 from aocHelpers.decorators import aoc_part
 from aocHelpers.init import init
+from itertools import groupby
 
 
 def sum_rows(data, operators):
+    # slower oneliner
+    # sum([eval(f"{operators[i]}".join(line) for i, line in enumerate)])
+
     s = 0
     for i, v in enumerate(operators):
         if v == "+":
@@ -26,27 +31,21 @@ def part1(arr):
             lines[i].append(int(num))
     return sum_rows(lines, (i for i in arr[-1].split()))
 
+
 @aoc_part
 def part2(arr):
-    lines = []
+    operators = (i for i in arr[-1].split())
+    lines = transpose(arr[:-1])
+    lines = ["".join(i).strip() for i in lines]
 
-    row = []
-    i = 0
-    while i < len(arr[0]):
-        num = []
-        for l in arr[:-1]:
-            c = l[i]
-            if c.isdigit():
-                num.append(c)
-        if len(num) > 0:
-            row.append(int("".join(num)))
-        else:
-            lines.append(row)
-            row = []
-        i += 1
-    lines.append(row)
+    groups = [
+        [int(line) for line in group]
+        for key, group in groupby(lines, key=lambda x: x != "")
+        if key
+    ]
 
-    return sum_rows(lines, (i for i in arr[-1].split()))
+    return sum_rows(groups, operators)
+
 
 def parse_input(raw):
     return raw.splitlines()
